5d4cc87908effb3e6262bae8f8656fa7ed1be01b
devyadav <devashish18yadav@gmail.com>
2016-03-22 15:32:19 +0530

    Changed: Login is now a User Control.

---
+++ MessManagement/Login.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MessManagement
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : UserControl
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void button_login_Click(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new template());
+        }
+    }
+}

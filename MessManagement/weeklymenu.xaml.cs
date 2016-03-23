aaf2531d9fe1673bccc10566280b84e7d814d039
devyadav <devashish18yadav@gmail.com>
2016-03-23 22:40:09 +0530

    Add individual button contol func for monday meals.

--- MessManagement/weeklymenu.xaml.cs
+++ MessManagement/weeklymenu.xaml.cs
@@ -22,6 +22,7 @@ namespace MessManagement
     {
         int MaxExtras = 4;
         Dictionary<String, Dictionary<String, List<DayMenu>>> weeklymenu = new Dictionary<String, Dictionary<String, List<DayMenu>>>();
+
         public WeeklyMenu()
         {
             InitializeComponent();
@@ -29,7 +30,7 @@ namespace MessManagement
 
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void button_add00_Click(object sender, RoutedEventArgs e)
         {
             Menu00.CommitEdit();
             if (weeklymenu["0"]["0"].Count < MaxExtras)
@@ -42,7 +43,32 @@ namespace MessManagement
                 Menu00.Items.Refresh();
         }
 
-        private void button_remove_Click(object sender, RoutedEventArgs e)
+        private void button_add01_Click(object sender, RoutedEventArgs e)
+        {
+            Menu01.CommitEdit();
+            if (weeklymenu["0"]["1"].Count < MaxExtras)
+            {
+                weeklymenu["0"]["1"].Add(new DayMenu() { Name = "New Item", Price = 0 });
+                Menu01.Items.Refresh();
+
+            }
+            else
+                Menu01.Items.Refresh();
+        }
+
+        private void button_add02_Click(object sender, RoutedEventArgs e)
+        {
+            Menu02.CommitEdit();
+            if (weeklymenu["0"]["2"].Count < MaxExtras)
+            {
+                weeklymenu["0"]["2"].Add(new DayMenu() { Name = "New Item", Price = 0 });
+                Menu02.Items.Refresh();
+
+            }
+            else
+                Menu02.Items.Refresh();
+        }
+        private void button_remove00_Click(object sender, RoutedEventArgs e)
         {
             Menu00.CommitEdit();
             if (Menu00.SelectedItems.Count > 0)
@@ -62,14 +88,61 @@ namespace MessManagement
                 }
             }
         }
+        private void button_remove01_Click(object sender, RoutedEventArgs e)
+        {
+            Menu01.CommitEdit();
+            if (Menu01.SelectedItems.Count > 0)
+            {
+                try
+                {
+                    var selectedIndex = Menu01.SelectedIndex;
+                    weeklymenu["0"]["1"].RemoveAt(selectedIndex);
+                }
+                catch (Exception exception)
+                {
+                    Console.Write("\n\n" + exception.ToString() + "\n\n");
+                }
+                finally
+                {
+                    Menu01.Items.Refresh();
+                }
+            }
+        }
+        private void button_remove02_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Menu02.CommitEdit();
+            if (Menu02.SelectedItems.Count > 0)
+            {
+                try
+                {
+                    var selectedIndex = Menu02.SelectedIndex;
+                    weeklymenu["0"]["2"].RemoveAt(selectedIndex);
+                }
+                catch (Exception exception)
+                {
+                    Console.Write("\n\n" + exception.ToString() + "\n\n");
+                }
+                finally
+                {
+                    Menu02.Items.Refresh();
+                }
+            }
+        }
+
         private void LoadMenus()
         {
             weeklymenu.Add("0", new Dictionary<String, List<DayMenu>>());
             weeklymenu["0"].Add("0", new List<DayMenu>());
-            weeklymenu["0"]["0"].Add(new DayMenu() { Name = "Rasgulle", Price = 14 });
-            weeklymenu["0"]["0"].Add(new DayMenu() { Name = "GulabJamun", Price = 20 });
-            //Console.Write("\n\n" + +"\n\n");
+            weeklymenu["0"].Add("1", new List<DayMenu>());
+            weeklymenu["0"].Add("2", new List<DayMenu>());
+
+            weeklymenu["0"]["0"].Add(new DayMenu() { Name = "Rasgulla", Price = 14 });
+            weeklymenu["0"]["1"].Add(new DayMenu() { Name = "GulabJamun", Price = 20 });
+            weeklymenu["0"]["2"].Add(new DayMenu() { Name = "ChikenCurry", Price = 25 });
             Menu00.ItemsSource = weeklymenu["0"]["0"];
+            Menu01.ItemsSource = weeklymenu["0"]["1"];
+            Menu02.ItemsSource = weeklymenu["0"]["2"];
         }
 
 

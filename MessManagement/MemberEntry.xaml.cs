facc84d0550e7212afdea6684db207e7e8424365
devyadav <devashish18yadav@gmail.com>
2016-03-24 15:36:21 +0530

    Added: (Will be Removed in Future)StrikeThrough using background color if required.

--- MessManagement/MemberEntry.xaml.cs
+++ MessManagement/MemberEntry.xaml.cs
@@ -20,9 +20,43 @@ namespace MessManagement
     /// </summary>
     public partial class MemberEntry : UserControl
     {
+        List<DayMenu> todayextralist = new List<DayMenu>();
+
         public MemberEntry()
         {
             InitializeComponent();
+            LoadExtras();
         }
+        private void LoadExtras()
+        {
+            todayextralist.Add(new DayMenu() { Name = "Rasgulla", Price = 14 });
+            todayextralist.Add(new DayMenu() { Name = "Chicken Curry", Price = 14 });
+            menutoday.ItemsSource = todayextralist;
+        }
+        
+        private void button_strike(object sender, RoutedEventArgs e)
+        {
+            menutoday.CommitEdit();
+            if (menutoday.SelectedItems.Count > 0)
+            {
+                try
+                {
+                    var rowIndex = menutoday.SelectedIndex;
+                    var row = (DataGridRow)menutoday.ItemContainerGenerator.ContainerFromIndex(rowIndex);
+                    Console.Write("\n\n" + row.GetType() + "\n\n");
+                    row.Background = Brushes.Black;
+                    
+                }
+                catch (Exception exception)
+                {
+                    Console.Write("\n\n" + exception.ToString() + "\n\n");
+                }
+                finally
+                {
+                    //menutoday.Items.Refresh();
+                }
+            }
+        }
+
     }
 }

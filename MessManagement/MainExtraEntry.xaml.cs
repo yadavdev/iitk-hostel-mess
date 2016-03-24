8e9257357dc2d4b2a3f7a2c5b875ee9369386721
devyadav <devashish18yadav@gmail.com>
2016-03-25 04:35:31 +0530

    Added: Source to datagrids.

--- MessManagement/MainExtraEntry.xaml.cs
+++ MessManagement/MainExtraEntry.xaml.cs
@@ -20,14 +20,46 @@ namespace MessManagement
     /// </summary>
     public partial class MainExtraEntry : UserControl
     {
+        List<DailyMenuEntry> today_fixed = new List<DailyMenuEntry>();
+        List<DailyMenuEntry> today_special = new List<DailyMenuEntry>();
         public MainExtraEntry()
         {
             InitializeComponent();
+            LoadExtras();
+            todayspecialextra.ItemsSource = today_special;
+            fixedextra.ItemsSource = today_fixed;
         }
 
         private void button_back_Click(object sender, RoutedEventArgs e)
         {
             Switcher.Switch(new MemberEntry());
         }
+
+        private void button_enter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void LoadExtras()
+        {
+            today_special.Add(new DailyMenuEntry() { Name = "Chicken Curry", Quantity = 0, Price = 86 });
+            today_special.Add(new DailyMenuEntry() { Name = "Veg Curry", Quantity = 0, Price = 345 });
+            today_special.Add(new DailyMenuEntry() {  Name = "Hall2 Special Curry", Quantity = 0, Price = 104 });
+            today_special.Add(new DailyMenuEntry() { Name = "Hall2 Special Curry", Quantity = 0, Price = 104 });
+            today_special.Add(new DailyMenuEntry() { Name = "Hall2 Special Curry", Quantity = 0, Price = 104 });
+
+            today_fixed.Add(new DailyMenuEntry() { Name = "Chicken Curry", Quantity = 0, Price = 86 });
+            today_fixed.Add(new DailyMenuEntry() { Name = "Veg Curry", Quantity = 0, Price = 345 });
+            today_fixed.Add(new DailyMenuEntry() { Name = "Hall2 Special Curry", Quantity = 0, Price = 104 });
+            today_fixed.Add(new DailyMenuEntry() { Name = "Chicken Curry", Quantity = 0, Price = 86 });
+            today_fixed.Add(new DailyMenuEntry() { Name = "Veg Curry", Quantity = 0, Price = 345 });
+            today_fixed.Add(new DailyMenuEntry() { Name = "Hall2 Special Curry", Quantity = 0, Price = 104 });
+        }
+    }
+
+    public class DailyMenuEntry
+    {
+        public string Name { get; set; }
+        public uint Quantity { get; set; }
+        public int Price { get; set; }
     }
 }

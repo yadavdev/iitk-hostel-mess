a4b4bd62c1b6aa2e3f63f81f5fdf991deb5540d9
devyadav <devashish18yadav@gmail.com>
2016-03-24 18:20:08 +0530

    Added: Business Logic to back button and datagrid.

--- MessManagement/LatestMemberTransactions.xaml.cs
+++ MessManagement/LatestMemberTransactions.xaml.cs
@@ -20,17 +20,65 @@ namespace MessManagement
     /// </summary>
     public partial class LatestMemberTransactions : UserControl
     {
+        List<MemberTransaction> trans_list = new List<MemberTransaction>();
+
         public LatestMemberTransactions()
         {
             InitializeComponent();
+            LoadTrans();
         }
+
         private void button_remove_trans_Click(object sender, RoutedEventArgs e)
         {
-            Console.Write("\n\nTransaction Removed.\n\n");
+            latestmembertransactions.CommitEdit();
+            try
+            {
+                Console.WriteLine("Transaction Removed.");
+                trans_list[latestmembertransactions.SelectedIndex].Price = 0;
+                trans_list[latestmembertransactions.SelectedIndex].Quantity = 0;
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine("Error in removing Latest Transaction.");
+                Console.WriteLine(exception);
+
+            }
+            latestmembertransactions.Items.Refresh();
         }
+
         private void button_back_Click(object sender, RoutedEventArgs e)
         {
+            UpdateDatabase();
             Switcher.Switch(new MemberEntry());
         }
+
+        private void LoadTrans()
+        {
+            LoadFromDatabase();
+            latestmembertransactions.ItemsSource = trans_list;
+        }
+
+        private void LoadFromDatabase()
+        {
+            trans_list.Add(new MemberTransaction() { SNo = 1, RollNo = 11001, Name = "Chicken Curry", Quantity = 1, Price = 86 });
+            trans_list.Add(new MemberTransaction() { SNo = 2, RollNo = 14073, Name = "Veg Curry", Quantity = 4, Price = 345 });
+            trans_list.Add(new MemberTransaction() { SNo = 3, RollNo = 11342, Name = "Hall2 Special Curry", Quantity = 2, Price = 104 });
+        }
+
+        private void UpdateDatabase()
+        {
+
+        }
+
     }
+
+    public class MemberTransaction
+    {
+        public int SNo { get; set; }
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+    }
+
 }

5521df769fa6f43873de4b47b74296af23299b49
devyadav <devashish18yadav@gmail.com>
2016-03-27 04:18:31 +0530

    Add: Skeleton for load from database and save to database and other functions.

--- MessManagement/EditMember.xaml.cs
+++ MessManagement/EditMember.xaml.cs
@@ -30,12 +30,24 @@ namespace MessManagement
 
         private void button_back_Click(object sender, RoutedEventArgs e)
         {
-
+            //Switcher.Switch(new ____());
         }
 
         private void button_save_Click(object sender, RoutedEventArgs e)
         {
             editmember.CommitEdit();
+            int success = SaveToDatabase();
+            if (success == 1)
+            {
+                //Show A POPUP THAT SAVED SUCCESSFULLY
+                //LOAD FROM DATABASE AND ASK TO CHECK THAT CHANGES ARE REFLECTED
+                int loadsuccess = LoadFromDatabase();
+                editmember.Items.Refresh();
+            }
+            else
+            {
+                //SHOW A POPUP/IN A LABEL THAT SAVE FAILED and WHAT TO DO NEXT
+            }
         }
 
         private void button_add_Click(object sender, RoutedEventArgs e)
@@ -48,15 +60,34 @@ namespace MessManagement
             editmember.CommitEdit();
         }
 
+        private void button_reload_Click(object sender, RoutedEventArgs e)
+        {
+            editmember.CommitEdit();
+            int loadsuccess = LoadFromDatabase();
+            editmember.Items.Refresh();
+        }
+
         private void LoadMemberList()
+        {
+            int success = LoadFromDatabase();
+            editmember.ItemsSource = memberlist; //Can't be changed in runtime. Change list instead and refresh
+        }
+
+        private int LoadFromDatabase()
         {
             memberlist.Add(new EditMemberList() { RollNo = 13001, Name = "John Legends", Remarks = "Hall-2" });
             memberlist.Add(new EditMemberList() { RollNo = 13048, Name = "Ranganathan Srinivasan", Remarks = "Hall-3" });
             memberlist.Add(new EditMemberList() { RollNo = 13049, Name = "Ranganathan Mutthunathan", Remarks = "Hall-5" });
             memberlist.Add(new EditMemberList() { RollNo = 13050, Name = "Terminator Adams", Remarks = "Hall-10" });
-            editmember.ItemsSource = memberlist;
+
+            return 1;
         }
 
+        private int SaveToDatabase()
+        {
+            //TO DO: Save only when changes noticed in the list (Add a check maybe)
+            return 1;
+        }
     }
     public class EditMemberList
     {

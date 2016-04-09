2e44472e85a508f2f51a36b9a7b695ca7bf533f1
devyadav <devashish18yadav@gmail.com>
2016-04-10 01:52:30 +0530

    Add: dropdown layout for reference.

---
+++ MessManagement/dropdown.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MessManagement
+{
+    /// <summary>
+    /// Interaction logic for dropdown.xaml
+    /// </summary>
+    public partial class dropdown : UserControl
+    {
+        List<DayChoose> todaylist = new List<DayChoose>();
+        public dropdown()
+        {
+            InitializeComponent();
+            todaylist.Add(new DayChoose() { dayname = "Monday" });
+            todaylist.Add(new DayChoose() { dayname = "Tuesday" });
+            todaylist.Add(new DayChoose() { dayname = "Wednesday" });
+            todaylist.Add(new DayChoose() { dayname = "Thursday" });
+            todaylist.Add(new DayChoose() { dayname = "Friday" });
+            todaylist.Add(new DayChoose() { dayname = "Saturday" });
+            todaylist.Add(new DayChoose() { dayname = "Sunday" });
+            comboboxlist_day.ItemsSource = todaylist;
+            comboboxlist_day.DisplayMemberPath = "dayname";
+            comboboxlist_day.SelectedValuePath = "dayname";
+            comboboxlist_day.SelectedValue = "Monday";
+        }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}

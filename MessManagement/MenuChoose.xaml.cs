0785bfe7fa046bafc9b0e974a1626c41584ad1f3
devyadav <devashish18yadav@gmail.com>
2016-04-10 01:50:31 +0530

    Add: Data binding to list for drop down lists.

--- MessManagement/MenuChoose.xaml.cs
+++ MessManagement/MenuChoose.xaml.cs
@@ -20,9 +20,55 @@ namespace MessManagement
     /// </summary>
     public partial class MenuChoose : UserControl
     {
+        List<DayChoose> todaylist = new List<DayChoose>();
+        List<DayChoose> todaylist2 = new List<DayChoose>();
         public MenuChoose()
         {
             InitializeComponent();
+            todaylist.Add(new DayChoose() { dayname = "Monday" });
+            todaylist.Add(new DayChoose() { dayname = "Tuesday" });
+            todaylist.Add(new DayChoose() { dayname = "Wednesday" });
+            todaylist.Add(new DayChoose() { dayname = "Thursday"  });
+            todaylist.Add(new DayChoose() { dayname = "Friday" });
+            todaylist.Add(new DayChoose() { dayname = "Saturday" });
+            todaylist.Add(new DayChoose() { dayname = "Sunday" });
+            todaylist2.Add(new DayChoose() { dayname = "Breakfast" });
+            todaylist2.Add(new DayChoose() { dayname = "Lunch" });
+            todaylist2.Add(new DayChoose() { dayname = "Dinner" });
+            comboboxlist_day.ItemsSource = todaylist;
+            comboboxlist_day.DisplayMemberPath = "dayname";
+            comboboxlist_day.SelectedValuePath = "dayname";
+            comboboxlist_day.SelectedValue = "Monday";
+            comboboxlist_meal.ItemsSource = todaylist2;
+            comboboxlist_meal.DisplayMemberPath = "dayname";
+            comboboxlist_meal.SelectedValuePath = "dayname";
+            comboboxlist_meal.SelectedValue = "Breakfast";
         }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void button_continue_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void button_remove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void button_back_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void button_add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+    public class DayChoose
+    {
+        public string dayname { get; set; }
     }
 }

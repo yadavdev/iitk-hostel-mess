d1d5338a014d9aad623b3e8b15b4a09f3d62bbc4
Gaurav13274 <gaurav21776@gmail.com>
2016-04-10 13:30:17 +0530

    database binding started

--- MessManagement/MainWindow.xaml.cs
+++ MessManagement/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace MessManagement
         {
             InitializeComponent();
             Switcher.pageSwitcher = this;
-            Switcher.Switch(new MemberDuesGenerate());
+            Switcher.Switch(new MemberEntry());
         }
         public void Navigate(UserControl nextPage)
         {

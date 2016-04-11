f7337e3254e0f943fdea78334f14a0cc45a42190
Devashish Kumar Yadav <devashish18yadav@gmail.com>
2016-04-11 13:54:13 +0530

    Add: populate sample data table.

---
+++ Database/database_sample_populate.sql
@@ -0,0 +1,4 @@
+insert into mess_db.smt (roll,item,quantity,price) values (13001,"Chicken Curry", 1,65);
+insert into mess_db.smt (roll,item,quantity,price) values (13201,"Veg Curry", 1,35);
+insert into mess_db.smt (roll,item,quantity,price) values (13432,"Rasgulle", 2,16);
+select * FROM mess_db.smt;
---
+++ Database/database_student_populate.sql
@@ -0,0 +1,4 @@
+INSERT into mess_db.student (name,roll) values ("John Adams",13001 );
+INSERT into mess_db.student (name,roll) values ("John Lenons",13201 );
+INSERT into mess_db.student (name,roll) values ("John Lahri",13432 );
+INSERT into mess_db.student (name,roll) values ("Ram Manohar",13422 );

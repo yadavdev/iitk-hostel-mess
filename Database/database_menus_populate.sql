7fbd8797cbb4a71ef82987479fb04ca42b6d5150
Devashish Kumar Yadav <devashish18yadav@gmail.com>
2016-04-11 16:14:14 +0530

    Add: new file

---
+++ Database/database_menus_populate.sql
@@ -0,0 +1,3 @@
+insert into mess_db.fixedmenu (article, rate) values ("Egg", 6);
+insert into mess_db.fixedmenu (article, rate) values ("Milk", 12);
+insert into mess_db.fixedmenu (article, rate) values ("Paneer Bhujia", 15);
